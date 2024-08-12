db45f934ea7e6523916a085e803cb24950855217
tomohiro_0225 <express22212270@gmail.com>
2024-08-12 10:12:28 +0900

    flask migrateでDBのカラム追加。

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+Migrate(app, db)
 
 # テーブルの設定
 class User(db.Model):
@@ -23,11 +25,13 @@ class User(db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    administrator = db.Column(db.String(1))
 
-    def __init__(self, email, username, password_hash):
+    def __init__(self, email, username, password_hash, administrator):
         self.email = email
         self.username = username
         self.password_hash = password_hash
+        self.administrator = administrator
 
     def __repr__(self):
         return f"Username: {self.username}"
